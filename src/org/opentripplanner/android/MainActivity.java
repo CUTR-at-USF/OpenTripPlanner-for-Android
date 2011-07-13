56e4f1b4ba3ac8daf9cd21cd067ac04c3803675c
Marcy <marchica@gmail.com>
2011-07-13 21:19:54 +0000

    Added license notices and did a bit of cleanup

--- src/org/opentripplanner/android/MainActivity.java
+++ src/org/opentripplanner/android/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Marcy Gordon
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.opentripplanner.android;
 
 import java.io.IOException;
@@ -164,8 +180,6 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
 				builder.setItems(items, new DialogInterface.OnClickListener() {
 				    
 					public void onClick(DialogInterface dialog, int item) {
-				        //Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
-				        
 				        if(items[item].equals("Current Location")) {
 				        	if(buttonID == R.id.btnStartLocation) {
 				        		tbStartLocation.setText("My Location");
@@ -415,23 +429,13 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
 
 	private ServiceConnection mConnection = new ServiceConnection() {
 	    public void onServiceConnected(ComponentName className, IBinder service) {
-	        // This is called when the connection with the service has been
-	        // established, giving us the service object we can use to
-	        // interact with the service.  Because we have bound to a explicit
-	        // service that we know is running in our own process, we can
-	        // cast its IBinder to a concrete class and directly access it.
 	        mBoundService = ((NavigationService.LocalBinder)service).getService();
 
-	        // Tell the user about this for our demo.
 	        Toast.makeText(MainActivity.this, "Connected service!",
 	                Toast.LENGTH_SHORT).show();
 	    }
 
 	    public void onServiceDisconnected(ComponentName className) {
-	        // This is called when the connection with the service has been
-	        // unexpectedly disconnected -- that is, its process crashed.
-	        // Because it is running in our same process, we should never
-	        // see this happen.
 	        mBoundService = null;
 	        Toast.makeText(MainActivity.this, "Disconnected service!",
 	                Toast.LENGTH_SHORT).show();
@@ -439,10 +443,6 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
 	};
 
 	void doBindService() {
-	    // Establish a connection with the service.  We use an explicit
-	    // class name because we want a specific service implementation that
-	    // we know will be running in our own process (and thus won't be
-	    // supporting component replacement by other applications).
 	    bindService(new Intent(MainActivity.this, 
 	            NavigationService.class), mConnection, Context.BIND_AUTO_CREATE);
 	    mIsBound = true;
@@ -450,7 +450,6 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
 
 	void doUnbindService() {
 	    if (mIsBound) {
-	        // Detach our existing connection.
 	        unbindService(mConnection);
 	        mIsBound = false;
 	    }
