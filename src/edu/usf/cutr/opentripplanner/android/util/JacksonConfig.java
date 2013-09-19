d8eaf5d71f0da2176549bc5f0ffaf6d25bc29d1d
Sean J. Barbeau <barbeau@cutr.usf.edu>
2013-09-19 14:04:54 -0400

    Fix #36 - Sets Jackson to ignore unknown properties when parsing API responses.

--- src/edu/usf/cutr/opentripplanner/android/util/JacksonConfig.java
+++ src/edu/usf/cutr/opentripplanner/android/util/JacksonConfig.java
@@ -216,10 +216,7 @@ public class JacksonConfig {
 		    mapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
 		    mapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true);
 		    mapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true);
-
-			// Tell Jackson to expect the JSON in PascalCase, instead of
-			// camelCase
-			//mapper.setPropertyNamingStrategy(new PropertyNamingStrategy.PascalCaseStrategy());
+		    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		}
 		return mapper;
 	}
