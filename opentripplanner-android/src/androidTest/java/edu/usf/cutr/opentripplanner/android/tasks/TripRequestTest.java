c87fd5da0d157768bd27077ec8220bc7e58b9c5a
vreixo <vreixo@gmail.com>
2014-09-07 19:00:27 +0200

    Added tests.

---
+++ opentripplanner-android/src/androidTest/java/edu/usf/cutr/opentripplanner/android/tasks/TripRequestTest.java
@@ -0,0 +1,14 @@
+package edu.usf.cutr.opentripplanner.android.tasks;
+
+import junit.framework.TestCase;
+
+public class TripRequestTest extends TestCase {
+
+    public void testGetErrorMessage() throws Exception {
+
+    }
+
+    public void testRequestPlan() throws Exception {
+
+    }
+}
