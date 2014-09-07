58949b07697d2eee723ffe07be36efae055c3231
vreixo <vreixo@gmail.com>
2014-09-08 01:08:30 +0200

    Fix #257 - Request filled manually instead of loading an stored json file. There were issues parsing modes and other fields correctly.

--- opentripplanner-android/src/androidTest/java/edu/usf/cutr/opentripplanner/android/tasks/TripRequestTest.java
+++ opentripplanner-android/src/androidTest/java/edu/usf/cutr/opentripplanner/android/tasks/TripRequestTest.java
@@ -5,7 +5,6 @@ import android.content.res.Resources;
 import android.test.ActivityTestCase;
 import android.text.format.DateFormat;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import junit.framework.Assert;
@@ -38,30 +37,43 @@ public class TripRequestTest extends ActivityTestCase {
     public void testRequestPlan() throws Exception {
         Context context = getInstrumentation().getTargetContext();
         Resources resources = context.getResources();
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        InputStream inputStreamSavedResponse = resources.openRawResource(R.raw.test_trip_response_corunha);
-        InputStream inputStreamSavedRequest = resources.openRawResource(R.raw.test_trip_request_corunha);
-
-        Response savedResponse = mapper.readValue(inputStreamSavedResponse, Response.class);
-        Request savedRequest = mapper.readValue(inputStreamSavedRequest, Request.class);
-
+        InputStream inputStream = resources.openRawResource(R.raw.test_trip_response_corunha);
+        Response savedResponse = JacksonConfig.getObjectReaderInstance().readValue(inputStream);
         String baseURL = "http://galadriel.dc.fi.udc.es:8080/otp";
+        Request request = new Request();
+        double startLatitude = 43.3527384041191, startLlongitude = -8.409261703491211;
+        double endLatitude = 43.33304453039103, endLongitude = -8.411107063293457;
+        boolean arriveBy = false;
+        TraverseModeSet modes = new TraverseModeSet(TraverseMode.BUS, TraverseMode.WALK);
+        OptimizeType optimization = OptimizeType.QUICK;
+        String startLocation = startLatitude + "," + startLlongitude;
+        String endLocation = endLatitude + "," + endLongitude;
 
-        Request newRequest = new Request();
-        newRequest.setFrom(savedRequest.getFrom());
-        newRequest.setTo(savedRequest.getTo());
-        newRequest.setOptimize(savedRequest.getOptimize());
-        newRequest.setModes(savedRequest.getModes());
-        newRequest.setDateTime(savedRequest.getDateTime());
-
-        TripRequest tripRequest = new TripRequest(null, context, resources, null, null);
-        Response newResponse = tripRequest.requestPlan(newRequest, OTPApp.FOLDER_STRUCTURE_PREFIX_NEW, baseURL);
-
-        if (newResponse.getPlan() != null && newResponse.getPlan().getItinerary() != null && newResponse.getPlan().getItinerary().get(0) != null){
-            TripPlan tripPlan = newResponse.getPlan();
+        try {
+            request.setFrom(URLEncoder.encode(startLocation, OTPApp.URL_ENCODING));
+            request.setTo(URLEncoder.encode(endLocation, OTPApp.URL_ENCODING));
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        request.setOptimize(optimization);
+        request.setModes(modes);
+        Date requestTripDate;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2014, Calendar.APRIL, 9, 18, 4);
+        requestTripDate = calendar.getTime();
+        request.setDateTime(
+                DateFormat.format(OTPApp.FORMAT_OTP_SERVER_DATE_QUERY,
+                        requestTripDate.getTime()).toString(),
+                DateFormat
+                        .format(OTPApp.FORMAT_OTP_SERVER_TIME_QUERY, requestTripDate.getTime())
+                        .toString());
+        TripRequest tripRequest = new TripRequest(null, getActivity(), resources, null, null);
+        Response newResponse = tripRequest.requestPlan(request, OTPApp.FOLDER_STRUCTURE_PREFIX_NEW, baseURL);
+        TripPlan tripPlan = newResponse.getPlan();
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File(context.getFilesDir(), "test_trip_request_corunha.json");
+        mapper.writeValue(file, request);
+        if (tripPlan != null && tripPlan.getItinerary() != null && tripPlan.getItinerary().get(0) != null){
             Itinerary itinerary = tripPlan.getItinerary().get(0);
             Assert.assertEquals(itinerary.legs.get(0).getSteps().get(0).getLat(), savedResponse.getPlan().getItinerary().get(0).legs.get(0).getSteps().get(0).getLat());
         }
